dd5f647ba72b8eaa6b9dd171fdd6dce70c570959
GokhulaKrishnan <gokhulakrishnant@gmail.com>
2026-01-30 18:28:31 -0500

    feat: added commader.js for parsing CLI commands

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,7 +1,25 @@
+import { Command } from "commander";
 import { login } from "./commands/login";
 import { logout } from "./commands/logout";
 import { status } from "./commands/status";
 
-// login();
-status();
-// logout();
+const program = new Command();
+
+program
+  .name("impact")
+  .description("Track your GitHub activity and generate career ready summaries")
+  .version("0.1.0");
+
+program
+  .command("login")
+  .description("Connect your GitHub account")
+  .action(login);
+
+program.command("status").description("Check login status").action(status);
+
+program
+  .command("logout")
+  .description("Disconnect your GitHub account")
+  .action(logout);
+
+program.parse();
